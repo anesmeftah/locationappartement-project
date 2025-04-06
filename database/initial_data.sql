986c95d68d31bc4d3e8cffebf6eb4b7d869872f9
Anas <anesmeftah03@gmail.com>
2025-04-06 11:37:06 +0100

    FIX DB

--- database/initial_data.sql
+++ database/initial_data.sql
@@ -1,5 +1,5 @@
 USE locationappartement;
--- Insert into CLIENTS table
+-- CLIENTS
 INSERT INTO CLIENTS (FirstName, LastName, email, ID, TELEPHONE, MDP)
 VALUES 
 ('John', 'Doe', 'john.doe@example.com', 1, '1234567890', 'password123'),
@@ -7,7 +7,7 @@ VALUES
 ('Alice', 'Johnson', 'alice.johnson@example.com', 3, '1122334455', 'password789'),
 ('Bob', 'Brown', 'bob.brown@example.com', 4, '6677889900', 'password111');
 
--- Insert into APPARTEMENT table
+-- APPARTEMENT
 INSERT INTO APPARTEMENT (ID, ADDRESS, PRIX, DESCRIP, STATUT)
 VALUES 
 (1, '123 Main St, Cityville', 500.00, 'A cozy one-bedroom apartment in the heart of the city.', 'available'),
@@ -15,7 +15,7 @@ VALUES
 (3, '789 Oak St, Villageton', 600.00, 'A modern one-bedroom apartment with all amenities.', 'available'),
 (4, '321 Pine St, Hamletburg', 900.00, 'A luxurious three-bedroom apartment with a swimming pool.', 'available');
 
--- Insert into RESERVATION table
+-- RESERVATION
 INSERT INTO RESERVATION (ID, ID_CLIENT, ID_APPARTEMENT, DATEDEBUT, DATEFIN, STATUT, PENALITE)
 VALUES 
 (1, 1, 1, '2025-05-01', '2025-05-10', 'confirmed', 0),
@@ -24,4 +24,9 @@ VALUES
 (4, 4, 4, '2025-08-01', '2025-08-20', 'confirmed', 0);
 
 
-SELECT * From appartement
+SELECT * From appartement;
+SELECT * FROM CLIENTS;
+
+INSERT INTO CLIENTS (FirstName, LastName, email, ID, TELEPHONE, MDP,class)
+VALUES 
+('anas', 'meftah', 'anas.meftah@example.com', 6, '+21697846830', 'password123',1);
