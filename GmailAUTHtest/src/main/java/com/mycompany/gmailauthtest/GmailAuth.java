34bff0bc9115104e8382fcb5c73d8fca648d25ea
Anas <anesmeftah03@gmail.com>
2025-04-17 09:48:39 +0100

    Refactor GmailAuth class for improved file handling and update settings for null analysis

--- GmailAUTHtest/src/main/java/com/mycompany/gmailauthtest/GmailAuth.java
+++ GmailAUTHtest/src/main/java/com/mycompany/gmailauthtest/GmailAuth.java
@@ -7,45 +7,26 @@ package com.mycompany.gmailauthtest;
 import java.util.Collections;
 import java.util.List;
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.auth.oauth2.AuthorizationCodeFlow;
-import com.google.api.client.auth.oauth2.StoredCredential;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.JsonParser;
-import com.google.api.client.json.Json;
-import com.google.api.services.gmail.Gmail;
-import com.google.api.services.gmail.model.*;
-
-import java.io.IOException;
-import java.io.InputStreamReader;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
-
 import com.google.api.services.gmail.Gmail;
 import com.google.api.services.gmail.GmailScopes;
+import com.google.api.services.gmail.model.ListMessagesResponse;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.FileInputStream;
 
 import java.security.GeneralSecurityException;
-import java.util.Collections;
-import java.util.List;
-
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 /**
  *
@@ -57,12 +38,14 @@ public class GmailAuth {
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
     private static final List<String> SCOPES = Collections.singletonList(GmailScopes.GMAIL_READONLY);
     private static GoogleClientSecrets getClientSecrets() throws IOException {
-            InputStream in = GmailAuth.class.getResourceAsStream(CLIENT_SECRET_FILE);
-            if (in == null) {
-                throw new FileNotFoundException("Resource not found: "+CLIENT_SECRET_FILE);
-            }
+        File clientSecretFile = new File(CLIENT_SECRET_FILE);
+        if (!clientSecretFile.exists()) {
+            throw new FileNotFoundException("File not found: " + CLIENT_SECRET_FILE);
+        }
+        try (InputStream in = new FileInputStream(clientSecretFile)) {
             return GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
         }
+    }
     public static void main(String[] args) throws Exception {
         // Build the authorization code flow
         System.out.println("in1");
